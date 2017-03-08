bd1e67bffa97dd90b1f08842804370def4b92d09
Xiaoqiang Wang <xiaoqiangwang@gmail.com>
2017-03-08 10:52:38 +0100

    separate _version module

--- caffi/__init__.py
+++ caffi/__init__.py
@@ -3,3 +3,4 @@ from __future__ import (print_function, absolute_import)
 from . import ca
 from .dbr import DBF, DBR
 from .constants import ECA, CA_OP, AlarmCondition, AlarmSeverity
+from ._version import __version__, version_info
